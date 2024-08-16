e58d2a6a87179b8a3d36d9739d8fb9a28dfcb7b0
Dominik Downarowicz <d.downarowicz@prodware.at>
2024-08-16 20:41:26 +0200

    Basic react-ts MSAL app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.tsx'
 import './index.css'
+import { useAuthProvider } from './auth/hooks/auth-provider.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const { AuthProvider } = useAuthProvider();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 )
